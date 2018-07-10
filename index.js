be38cfdec54bed81ff6bf220c39c48af11b2baea
Abigail Earl <asophieearl@gmail.com>
2018-07-10 18:14:57 +0000

    Added strike-through feature

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 //      $(this).closest("li").toggleclass("completed-todo")}
 //      $(#createbutton1).click(function(){
 $(document).ready(function(){
-    console.log("this works");
     $("#createbutton1").click(function(){
         var nameOfToDo=$("#write-to-do").val();
-console.log(nameOfToDo);
-var timeInMinutes=$("#minutesneeded").val();
-$("ul").append("<li><input type='checkbox'/>"+nameOfToDo+"<span>"+": "+timeInMinutes+" min."+"</span></li>");
-console.log(timeInMinutes);
-
-
+        var timeInMinutes=$("#minutesneeded").val();
+        $("ul").append("<li><input type='checkbox'/>"+nameOfToDo+"<span>: "+timeInMinutes+" min.</span></li>");
+        console.log(timeInMinutes);
     });
+    
+    $("input[type='checkbox']").click(function(){
+        $(this).closest("li").toggleClass("completed");
+    console.log("Working button");
+});
+    
 });
